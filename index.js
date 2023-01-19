110729eb6dbe41c046b6f51fb5a1b1855d39e39f
Craig Buchanan <craig.buchanan@cogniza.com>
2023-01-19 19:43:12 +0000

    adding searches adding creates

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const {
     config: authentication,
     befores = [],
     afters = [],
-  } = require('./authentication');
+} = require('./authentication');
   
-  module.exports = {
-    // This is just shorthand to reference the installed dependencies you have.
-    // Zapier will need to know these before we can upload.
-    version: require('./package.json').version,
-    platformVersion: require('zapier-platform-core').version,
-  
-    authentication,
-  
-    beforeRequest: [...befores],
-  
-    afterResponse: [...afters],
-  
-    // If you want your trigger to show up, you better include it here!
-    triggers: {},
-  
-    // If you want your searches to show up, you better include it here!
-    searches: {},
-  
-    // If you want your creates to show up, you better include it here!
-    creates: {},
-  
-    resources: {},
-  };
+const findEmployee = require('./searches/find_employee.js');
+const createEmployee = require('./creates/create_employee.js');
+
+module.exports = {
+  // This is just shorthand to reference the installed dependencies you have.
+  // Zapier will need to know these before we can upload.
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+
+  authentication,
+
+  beforeRequest: [...befores],
+  afterResponse: [...afters],
+
+  triggers: {},
+  searches: {
+    [findEmployee.key]: findEmployee
+  },
+  creates: {
+    [createEmployee.key]: createEmployee
+  },
+  resources: {},
+
+};
