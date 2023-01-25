54f40e9a637fc51d31b3888a9ffb06e95548a08a
Craig Buchanan <craig.buchanan@cogniza.com>
2023-01-25 14:07:48 +0000

    refactoring adding triggers adding additional creates adding additional searches

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const {
     befores = [],
     afters = [],
 } = require('./authentication');
-  
-const findEmployee = require('./searches/find_employee.js');
-const createEmployee = require('./creates/create_employee.js');
+
+// creates
+const createContact = require('./creates/contact_create.js');
+const saveContact = require('./creates/contact_save.js');
+
+const createEmployee = require('./creates/employee_create.js');
+const saveEmployee = require('./creates/employee_save.js');
+const setEmployeeStatus = require('./creates/employee_set_status.js');
+
+// searches
+const findContact = require('./searches/contact_find.js');
+const findEmployee = require('./searches/employee_find.js');
+
+// triggers
+const getLocations = require("./triggers/get_locations");
+const getDepartments = require("./triggers/get_departments");
 
 module.exports = {
-  // This is just shorthand to reference the installed dependencies you have.
-  // Zapier will need to know these before we can upload.
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
-
   authentication,
-
   beforeRequest: [...befores],
   afterResponse: [...afters],
-
-  triggers: {},
+  triggers: {
+    [getLocations.key]: getLocations,
+    [getDepartments.key]: getDepartments
+  },
   searches: {
+    [findContact.key]: findContact,
     [findEmployee.key]: findEmployee
   },
   creates: {
-    [createEmployee.key]: createEmployee
+    [createContact.key]: createContact,
+    [saveContact.key]: saveContact,
+
+    [createEmployee.key]: createEmployee,
+    [saveEmployee.key]: saveEmployee,
+    [setEmployeeStatus.key]: setEmployeeStatus
   },
   resources: {},
-
 };
